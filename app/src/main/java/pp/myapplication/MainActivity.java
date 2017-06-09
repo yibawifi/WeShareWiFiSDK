4dc2d549297951ae0d9426041db3b7ee21c05681
赵彦军 <362299465@qq.com>
2017-06-09 13:48:23 +0800

    第一次提交

---
+++ app/src/main/java/pp/myapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package pp.myapplication;
+
+import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.yiba.wifi.ui.WiFiActivity;
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById( R.id.tv).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              startActivity( new Intent( MainActivity.this , WiFiActivity.class ));
+            }
+        });
+    }
+}
